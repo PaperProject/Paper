de3777e0ce9029dd2e53f0afa130197765a0a602
hardrox <guyader35230@laposte.net>
2015-09-07 21:48:45 +0200

    MESSAGE QUE TU VEUX

---
+++ index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<!-- En-tête de la page -->
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="style.css" />
+		<title>Saint-Malo</title>
+	</head>
+
+	<body>
+
+			<div id="principale">
+				<header>
+					<div id="banniere">
+					
+						<img src="images/logo.png" alt="Logo" id="logo">
+						<h1> Accueil Saint-Malo </h1>
+						
+					</div>
+					
+					<nav>
+						<ul id="menu">
+						
+							<li><a href="accueil.html">Accueil</a></li>
+							<li>
+							<a ">Travaux Pratiques</a>
+							<ul>
+							<li><a href="#">Météo</a></li>
+							<li><a href="java.html">Java Script</a></li>
+							<li><a href="afficheur.html">Affichage Numérique</a></li>
+							<li><a href="bdd.php">Affichage base de données</a></li>
+							<li><a href="inscription.php">Inscription</a></li>
+							<li><a href="#">Météo en PHP</a></li>
+							
+						</ul>
+						</li>
+								<li><a href="#">A propos</a></li>
+								<li><a href="contact.html">Contact</a></li>
+						</ul>
+					</nav>
+
+				</header>
+
+				
+				<div id="banniere_image">
+					<!--<div id="banniere_description">
+						Voir la galerie Photos Bretagne (En Construction...)
+						<a href="galerie.html" class="bouton_rouge">Voir La Galerie <img src="images/flecheblanchedroite.png" alt="" /></a>
+					</div> -->
+				</div>
+
+				
+				<section>
+					<article>
+						<h1><img src="images/icone.png" alt="ico" class="icone" />Site en construction !</h1>
+						<p>Bonjour, le site est actuellement en construction et prend donc forme jours après jours.</p>
+						<p>Veuillez prendre en compte que nous sommes que des débutants et que celà nous a pris énormément de temps a le faire. </p>
+						<p>Cependant ils vous es quand même possible d'acceder à la météo qui est un peut plus haut.</p>
+						<p>Bonne visite sur notre site ! </p>
+					</article>
+		
+					<aside>
+							<h1>À propos du site</h1>
+							<img src="images/bulle.png" alt="" id="fleche_bulle" />
+							<p id="mauper"><img src="images/mauper.png" alt="mauper" /></p>
+							<p>Elèves en première année de BTS SNIR, nous faisons actuellement ce site internet dans le but que les personnes comme vous içi puisse se renseigner sur la météo actuelle. Nous vous souhaitons un agréable moment sur notre site qui appartient au Lycée Maupertuis à Saint-Malo en Bretagne. </p>
+							<p>Réseaux Sociaux :</p>
+							<a href="https://www.facebook.com/pages/Saint-Malo-Bretagne-France/107022542672026"target="_blank"><img src="images/facebook.png" alt="Facebook"/></a>
+							<a href="https://twitter.com/VilleSaintMalo"target="_blank"><img src="images/twitter.png" alt="twitter"/></a>
+							<a href="http://www.lycee-maupertuis.com/"target="_blank"><img src="images/logomauper2.png" alt="logomauper"/></a>
+			
+					</aside>
+				</section>
+	
+				<footer>
+					
+					<div id="photos">
+						<h1>Mes photos</h1>
+						<p> <a href="images/1.jpg"target="_blank"><img  src="images/foot2.jpg" alt="Photographie" /></a><a href="images/2.jpg"target="_blank"><img src="images/meteo2.jpg" alt="Photographie" /></a><a href="images/3.jpg"target="_blank"><img src="images/photo3.jpg" alt="Photographie" /></a>
+					</div>
+				
+				<div id="liens">
+                    <h1>Liens utiles</h1>
+                    <ul>
+                        <li><a href="http://www.google.fr" target="_blank">Google</a></li>
+                        <li><a href="http://www.youtube.com"target="_blank">YouTube</a></li>
+                        <li><a href="http://www.facebook.com" target="_blank">Facebook</a></li>
+                        <li><a href="http://www.twitter.com" target="_blank">Twitter</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="http://www.instagram.com"target="_blank">Instagram</a></li>
+                        <li><a href="http://www.spi0n.com"target="_blank">Spi0n</a> </li>
+                        <li><a href="http://www.lemonde.fr"target="_blank">Le Monde</a></li>
+                        <li><a href="http://www.ouest-france.fr"target="_blank">Ouest-France</a></li>
+                    </ul>
+                </div>
+				
+				<div id="copyright">
+                    <h1> Copyright ©</h1>
+                </div>
+				</footer>
+
+			</div>
+
+	</body>
+
+</html>
+
+
+
+
+
+
+<!--<audio class="player" src="musique.mp3" controls align="right-side"> </audio> -->
+
+
+<!--
+<ul>
+<A href="/cgi-bin/meteoweb.exe">(<IMG SRC="tete.png" align ="center">
+<form method="get" action="/cgi-bin/meteoweb.exe" >
+<input type="submit" name="nom1" value="Charger Données">
+</ul>   -->
